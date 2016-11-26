9a995dab2668a7d7bb13ac93c483a21435047d80
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2016-11-26 18:33:19 -0500

    Almost working cycle finding

--- src/problem95.rs
+++ src/problem95.rs
@@ -10,7 +10,7 @@ enum Aliquot {
 }
 
 pub fn solve() -> u64 {
-	let limit = 1_000;
+	let limit = 50;
 	let sieve = Sieve::new(limit);
 
 	let mut nums = vec![(1, Aliquot::Unknown); limit];
@@ -30,25 +30,41 @@ pub fn solve() -> u64 {
 		}
 	}
 
-	let mut stack = vec![];
+	for start in 2..limit {
+		let mut num = start;
+		let mut stack = vec![num];
 
-	for num in 2..limit {
-		stack.push(num);
+		loop {
+			let (mut next, mut status) = nums[num];
 
-		while !stack.is_empty() {
-			let (mut next, mut status) = nums[*stack.last().unwrap()];
+			println!("{:?} {} {:?}", stack, next, status);
 
 			if status == Aliquot::Unknown {
 				// calculate the divisor sum
 				next = sieve.factor(num).unwrap().into_iter().map(|(p, e)| (p.pow((e + 1) as u32) - 1) / (p - 1)).product::<usize>() - num;
 				status = Aliquot::Known;
+				nums[num] = (next, status);
 			}
 
-			nums[num] = (next, status);
+			match nums[next] {
+				(_, Aliquot::Unknown) | (_, Aliquot::Known) => {
+					if let Some(dist) = stack.iter().rposition(|&e| e == next) {
+						// found cycle
+						status = Aliquot::Cyclic(dist as u16 + 1);
+						nums[num] = (next, status);
+						break;
+					}
 
-			println!("{}, {}, {:?}", num, next, status);
+					stack.push(next);
+					num = next;
+					continue;
+				},
+				(_, Aliquot::Cyclic(k)) => { nums[num] = (next, Aliquot::Cyclic(k + 1)) },
+				(_, _) => break
 
-			stack.pop();
+			}
+
+			break;
 		}
 	}
 
